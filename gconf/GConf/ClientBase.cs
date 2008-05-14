e491608b2c3682c79ca79ee1b1207650dd3f7073
Stephane Delcroix <stephane@mono-cvs.ximian.com>
2008-05-14 15:21:04 +0000

    2008-05-14  Stephane Delcroix  <sdelcroix@novell.com>
    	* bootstrap-2.20: bump to 2.20.2
    
    	* gconf/GConf/gconf-sharp.dll.config.in:
    	* gconf/GConf/ClientBase.cs: call g_type_init during initialization.
    	Fixes bnc #389328.
    
    svn path=/trunk/gnome-sharp/; revision=103193

--- gconf/GConf/ClientBase.cs
+++ gconf/GConf/ClientBase.cs
@@ -37,6 +37,9 @@ namespace GConf
 	
 		[DllImport("gconf-2")]
 		static extern bool gconf_init (int argc, IntPtr argv, out IntPtr err);
+
+		[DllImport("libgobject-2.0-0.dll")]
+		static extern void g_type_init ();
 	
 		internal void Initialize ()
 		{
@@ -46,6 +49,7 @@ namespace GConf
 				gconf_init (0, IntPtr.Zero, out err);
 				if (err != IntPtr.Zero)
 					throw new GLib.GException (err);
+				g_type_init ();
 			}
 		}
 	}
